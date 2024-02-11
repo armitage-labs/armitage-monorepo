0b0a82a9db1255eaaf444392eb4039f42b739ec9
Gabriel Ferraz <sudoferraz@gmail.com>
2024-02-11 12:51:43 +0200

    Migrates sourcecred calculation into cred-manager

--- cred-manager/src/sourcecred/sourcecred.service.ts
+++ cred-manager/src/sourcecred/sourcecred.service.ts
@@ -1,15 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import {
+  CredGrainView,
+  ParticipantCredGrain,
+  sourcecred as sc,
+} from 'sourcecred';
+import { GitRepoService } from 'src/gitrepo/gitrepo.service';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { UserCredDto } from './types/userCredDto';
 import { executeCommand } from './utils/bashCommand';
 
 @Injectable()
 export class SourceCredService {
-  constructor(private readonly prismaService: PrismaService) {}
+  constructor(
+    private readonly prismaService: PrismaService,
+    private readonly gitRepoService: GitRepoService,
+  ) {}
 
-  async calculateCredScores(userId: string): Promise<UserCredDto> {
-    // const userRegisteredRepos = await this.retrieveGitReposByUser(userId);
-    return;
+  /**
+   * Calculates Cred scores for registered github repositories given
+   * a user id, outputting cred scores for all users that contributed
+   * on the registered repositories
+   **/
+  async calculateCredScores(userId: string): Promise<UserCredDto[]> {
+    const userRegisteredRepos = await this.gitRepoService.getByUser(userId);
+    const pluginConfigString = this.craftPluginConfigString(
+      userRegisteredRepos.map((repo) => repo.full_name),
+    );
+    await this.configureSourcecredGithubPlugin(pluginConfigString);
+    await this.loadSourceCredPlugins();
+    const userCredDtoArray = await this.loadLocalScInstance();
+    return userCredDtoArray;
   }
 
   craftPluginConfigString(githubReposFullNames: string[]): string {
@@ -34,5 +54,47 @@ export class SourceCredService {
     } catch (error) {
       console.error('configure repos command failed', error);
     }
+  }
+
+  async loadSourceCredPlugins() {
+    try {
+      await executeCommand(
+        "cd /Users/sudoferraz/Personal/source-cred/source-cred-instance \
+          && yarn clean-all \
+          && rm -r data/ledger.json \
+          && sed -i '' '5d' config/dependencies.json \
+          && yarn sourcecred go",
+      );
+    } catch (error) {
+      console.error('execute command failed', error);
+    }
+  }
+
+  async loadLocalScInstance(): Promise<UserCredDto[]> {
+    const HARDCODED_DIR =
+      '/Users/sudoferraz/Personal/source-cred/source-cred-instance';
+    const localInstance = await new sc.instance.LocalInstance(HARDCODED_DIR);
+    const graph: CredGrainView = await localInstance.readCredGrainView();
+    const userDataArray = this.processUserData(graph);
+    console.log(userDataArray.slice(0, 5));
+    return this.orderUserByCred(userDataArray);
+  }
+
+  processUserData(graph: CredGrainView): UserCredDto[] {
+    const participantsArray: ParticipantCredGrain[] = graph.participants();
+    const userDataArray: UserCredDto[] = participantsArray.map(
+      (participant) => {
+        return UserCredDto.fromJSON({
+          totalCred: participant.cred,
+          userName: participant.identity.name,
+          type: participant.identity.subtype.toString(),
+        });
+      },
+    );
+    return this.orderUserByCred(userDataArray);
+  }
+
+  orderUserByCred(userDtoArray: UserCredDto[]): UserCredDto[] {
+    return userDtoArray.sort((a, b) => b.totalCred - a.totalCred);
   }
 }
