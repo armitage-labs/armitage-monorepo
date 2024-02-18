b21a797dbdd2902dee40cc5001d628191d3f27f7
Gabriel Ferraz <sudoferraz@gmail.com>
2024-02-18 12:01:57 +0200

    Adds ui layout refactor

---
+++ src/constants/data.ts
@@ -0,0 +1,40 @@
+import { NavItem } from "@/types";
+
+export const navItems: NavItem[] = [
+  {
+    title: "Dashboard",
+    href: "/",
+    icon: "dashboard",
+    label: "Dashboard",
+  },
+  {
+    title: "GitRepo",
+    href: "/gitrepo",
+    icon: "user",
+    label: "user",
+  },
+  {
+    title: "Teams",
+    href: "/teams",
+    icon: "employee",
+    label: "employee",
+  },
+  {
+    title: "Profile",
+    href: "/create-team",
+    icon: "profile",
+    label: "profile",
+  },
+  {
+    title: "Info",
+    href: "/introduction",
+    icon: "kanban",
+    label: "kanban",
+  },
+  {
+    title: "Login",
+    href: "/",
+    icon: "login",
+    label: "login",
+  },
+];
