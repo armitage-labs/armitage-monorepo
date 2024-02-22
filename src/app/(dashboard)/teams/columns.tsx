0051845d7b9221666216ee07e5ddcf43b3216860
Alec <alecerasmus2@gmail.com>
2024-02-22 11:59:18 +0200

    Added team recalculation dialog (#16)

--- src/app/(dashboard)/teams/columns.tsx
+++ src/app/(dashboard)/teams/columns.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import axios from "axios";
 import { Button } from "@/components/ui/button";
 import { MoreHorizontal } from "lucide-react";
 import {
@@ -10,10 +11,22 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import {
+  AlertDialog,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 import { ColumnDef } from "@tanstack/react-table";
 import { ArrowUpDown } from "lucide-react";
+import { Circles } from "react-loader-spinner";
 import { Team } from "@/app/api/teams/fetchUserTeams";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export const teamsColumns: ColumnDef<Team>[] = [
   {
@@ -53,27 +66,63 @@ export const teamsColumns: ColumnDef<Team>[] = [
     cell: ({ row }) => {
       const router = useRouter();
       const team = row.original;
+      const [isLoading, setIsLoading] = useState(false);
+      const handleGenerateReport = async () => {
+        setIsLoading(true);
+        const { data } = await axios.get(`/api/credmanager?team_id=${team.id}`);
+        if (data && data.success) {
+          router.push(`/teams/${team.id}`);
+        } else {
+          setIsLoading(false);
+        }
+      };
+
       return (
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="ghost" className="h-8 w-8 p-0">
-              <span className="sr-only">Open menu</span>
-              <MoreHorizontal className="h-4 w-4" />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            <DropdownMenuLabel>Actions</DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem
-              onClick={() => {
-                router.push(`/teams/${team.id}`);
-              }}
-            >
-              View team details
-            </DropdownMenuItem>
-            <DropdownMenuItem>Calculate contributions</DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
+        <AlertDialog>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="ghost" className="h-8 w-8 p-0">
+                <span className="sr-only">Open menu</span>
+                <MoreHorizontal className="h-4 w-4" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuLabel>Actions</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem
+                onClick={() => {
+                  router.push(`/teams/${team.id}`);
+                }}
+              >
+                View team details
+              </DropdownMenuItem>
+              <AlertDialogTrigger asChild>
+                <DropdownMenuItem>Calculate contributions</DropdownMenuItem>
+              </AlertDialogTrigger>
+            </DropdownMenuContent>
+          </DropdownMenu>
+
+          <AlertDialogContent>
+            {isLoading ? (
+              <div className="flex justify-center">
+                <Circles color="black" />
+              </div>
+            ) : (
+              <div>
+                <AlertDialogHeader>
+                  <AlertDialogTitle>Arre you sure?</AlertDialogTitle>
+                  <AlertDialogDescription>
+                    This action can take several minutes to complete.
+                  </AlertDialogDescription>
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                  <AlertDialogCancel>Cancel</AlertDialogCancel>
+                  <Button onClick={handleGenerateReport}>Continue</Button>
+                </AlertDialogFooter>
+              </div>
+            )}
+          </AlertDialogContent>
+        </AlertDialog>
       );
     },
   },
