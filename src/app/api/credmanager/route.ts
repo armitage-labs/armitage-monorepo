ddd4abe2e515eadfe42029f3a5d0f46096f0088e
Gabriel Ferraz <sudoferraz@gmail.com>
2024-02-23 00:10:34 +0200

    Removes try catch from credmanager api to force frontend to break

--- src/app/api/credmanager/route.ts
+++ src/app/api/credmanager/route.ts
@@ -9,24 +9,24 @@ export interface UserCredDto {
 }
 
 export async function GET(req: NextRequest) {
-  try {
-    const teamId = req.nextUrl.searchParams.get("team_id");
-    const session = await getServerSession(options);
-    if (teamId) {
-      const CRED_MANAGER_ROUTE = process.env.CRED_MANAGER_ROUTE;
-      const calculatedUserCredDtos = await fetch(
-        `${CRED_MANAGER_ROUTE}/cred/team/${teamId}/${session?.accessToken}`,
-        {
-          method: "GET",
-        },
-      );
-      return NextResponse.json({
-        success: true,
-        userCredDtos: await calculatedUserCredDtos.json(),
-      });
-    }
-  } catch (error) {
-    console.error(error);
-    return NextResponse.json({ success: false, userCredDtos: [] });
+  // try {
+  const teamId = req.nextUrl.searchParams.get("team_id");
+  const session = await getServerSession(options);
+  if (teamId) {
+    const CRED_MANAGER_ROUTE = process.env.CRED_MANAGER_ROUTE;
+    const calculatedUserCredDtos = await fetch(
+      `${CRED_MANAGER_ROUTE}/cred/team/${teamId}/${session?.accessToken}`,
+      {
+        method: "GET",
+      },
+    );
+    return NextResponse.json({
+      success: true,
+      userCredDtos: await calculatedUserCredDtos.json(),
+    });
   }
+  // } catch (error) {
+  //   console.error(error);
+  //   return NextResponse.json({ success: false, userCredDtos: [] });
+  // }
 }
