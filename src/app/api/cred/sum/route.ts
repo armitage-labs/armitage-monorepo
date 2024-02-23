29a5b959a724a0f6d04d20f49fbba39863a7b3e5
Gabriel Ferraz <sudoferraz@gmail.com>
2024-02-23 21:38:04 +0200

    Removes try catch from cred/sum api

--- src/app/api/cred/sum/route.ts
+++ src/app/api/cred/sum/route.ts
@@ -4,15 +4,10 @@ import { options } from "../../auth/[...nextauth]/options";
 import { fetchCredSum } from "./fetchCredSum";
 
 export async function GET() {
-  try {
-    const session = await getServerSession(options);
-    if (session?.userId) {
-      const credSum = await fetchCredSum(session.userId);
-      return NextResponse.json({ success: true, credSum: credSum });
-    }
-    return NextResponse.json({ success: false, credSum: null });
-  } catch (error) {
-    console.error(error);
-    return NextResponse.json({ success: false, credSum: null });
+  const session = await getServerSession(options);
+  if (session?.userId) {
+    const credSum = await fetchCredSum(session.userId);
+    return NextResponse.json({ success: true, credSum: credSum });
   }
+  return NextResponse.json({ success: false, credSum: null });
 }
