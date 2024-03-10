b240bfb24ffc6dbe323bad3c4a99a6d52ce678de
Alec <alecerasmus2@gmail.com>
2024-03-10 23:45:52 +0100

    Added Controbution Interval on team page

---
+++ src/app/(dashboard)/teams/[teamId]/calculationIntervalChart.tsx
@@ -0,0 +1,45 @@
+import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+import * as React from "react";
+import {
+    Card,
+    CardContent,
+    CardHeader,
+    CardTitle,
+  } from "@/components/ui/card";
+
+interface CalculationIntervalChartProps {
+    intervals: any[];
+}
+
+export function CalculationIntervalChart({ intervals }: CalculationIntervalChartProps) {
+    return (
+        <div className="">
+        <div className=""></div>
+            <Card className="col-span-2">
+                <CardHeader>
+                    <CardTitle>Overview</CardTitle>
+                </CardHeader>
+                <CardContent className="pl-2">
+                    <ResponsiveContainer width="100%" height={350}>
+                        <AreaChart
+                            width={500}
+                            height={400}
+                            data={intervals}
+                        >
+                            <XAxis 
+                                dataKey="eTime"
+                                stroke="#888888"
+                                fontSize={12}
+                                tickLine={false}
+                                axisLine={false}
+                                tickFormatter={(value) => `${new Date(value).toDateString()}`}
+                            />
+                            <Tooltip />
+                            <Area type="monotone" dataKey="value" stroke="#888888" fill="#888888" />
+                        </AreaChart>
+                    </ResponsiveContainer>
+                </CardContent>
+            </Card>
+        </div>
+    );
+}
---
+++ src/app/api/contribution-calculation/fetchContributionCalculation.ts
@@ -0,0 +1,15 @@
+import { ContributionCalculation } from "@prisma/client";
+import prisma from "db";
+
+export async function fetchContributionCalculation(
+  teamId: string,
+): Promise<ContributionCalculation | null> {
+  const contributionCalculation = await prisma.contributionCalculation.findFirst({
+    where: {
+      Team: {
+        id: teamId,
+      },
+    },
+  });
+  return contributionCalculation;
+}
