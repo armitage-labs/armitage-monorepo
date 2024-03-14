d63fa7bfdadd50cec109b942f67a11d910f6764d
Gabriel Ferraz <sudoferraz@gmail.com>
2024-03-14 23:35:04 +0200

    Refreshes auth tokens when expired (#25)
    * Refreshes auth tokens when expired
    
    * Fixes linter issues
    
    * Removes debugging console logs

---
+++ src/app/sessionRefreshProvider.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { signIn, useSession } from "next-auth/react";
+import { useEffect } from "react";
+
+type Props = {
+  children?: React.ReactNode;
+};
+
+export const SessionRefreshProvider = ({ children }: Props) => {
+  const { data: session } = useSession();
+
+  useEffect(() => {
+    if (session?.error) {
+      if (session.error === "RefreshAccessTokenError") {
+        console.log("Refreshing the access token");
+        signIn("github", { callbackUrl: "/overview" });
+      }
+    }
+  }, [session]);
+
+  return <>{children}</>;
+};
