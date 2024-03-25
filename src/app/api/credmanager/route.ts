2ccca1f37149ae050a708aab0512c7dcd25d7cba
Alec <alecerasmus2@gmail.com>
2024-03-25 21:28:45 +0100

    Add event framework for backend (#64)
    * Add event framework for backend
    
    * Remove any
    
    * Linter issue
    
    * Add migration

--- src/app/api/credmanager/route.ts
+++ src/app/api/credmanager/route.ts
@@ -1,6 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { options } from "../auth/[...nextauth]/options";
 import { getServerSession } from "next-auth";
+import { EventName, createEvent } from "../event/createEvent";
 
 export interface UserCredDto {
   totalCred: number;
@@ -12,7 +13,7 @@ export async function GET(req: NextRequest) {
   // try {
   const teamId = req.nextUrl.searchParams.get("team_id");
   const session = await getServerSession(options);
-  if (teamId) {
+  if (teamId && session?.userId != null) {
     const CRED_MANAGER_ROUTE = process.env.CRED_MANAGER_ROUTE;
     const calculatedUserCredDtos = await fetch(
       `${CRED_MANAGER_ROUTE}/cred/team/${teamId}/${session?.accessToken}/${session?.user?.email}`,
@@ -20,6 +21,9 @@ export async function GET(req: NextRequest) {
         method: "GET",
       },
     );
+    createEvent(session?.userId, EventName.REQUEST_CALCULATION, {
+      teamId: teamId,
+    });
     return NextResponse.json({
       success: true,
       userCredDtos: calculatedUserCredDtos.json(),
---
+++ src/app/api/event/createEvent.ts
@@ -0,0 +1,20 @@
+import { Event } from "@prisma/client";
+import prisma from "db";
+
+export enum EventName {
+  REQUEST_CALCULATION,
+}
+
+export async function createEvent(
+  userId: string,
+  eventName: EventName,
+  eventData: object,
+): Promise<Event> {
+  return prisma.event.create({
+    data: {
+      user_id: userId,
+      event_name: EventName[eventName],
+      event_data: eventData,
+    },
+  });
+}
