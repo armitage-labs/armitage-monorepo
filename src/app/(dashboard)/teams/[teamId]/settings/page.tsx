9eb2dbb77ab00219d3eaf75a5b99b606bb25a1ba
Alec <alecerasmus2@gmail.com>
2024-03-26 21:14:41 +0100

    Add team name on settings page (#72)

--- src/app/(dashboard)/teams/[teamId]/settings/page.tsx
+++ src/app/(dashboard)/teams/[teamId]/settings/page.tsx
@@ -18,12 +18,18 @@ import {
 } from "@/components/ui/alert-dialog";
 import { useRouter } from "next/navigation";
 import TeamGithubRepositoriesTable from "@/components/teams/teamGithubRepositoriesTable";
+import { useEffect, useState } from "react";
+import { Team } from "@prisma/client";
+import { useSession } from "next-auth/react";
+import { Skeleton } from "@/components/ui/skeleton";
 
 interface PageProps {
   params: { teamId: string };
 }
 
 export default function TeamSettingsPage({ params }: PageProps) {
+  const [team, setTeam] = useState<Team>();
+  const { data: session } = useSession();
   const teamId = params.teamId;
   const breadcrumbItems = [
     { title: "Teams", link: "/teams" },
@@ -40,16 +46,33 @@ export default function TeamSettingsPage({ params }: PageProps) {
     }
   };
 
+  const handleFetchTeams = async () => {
+    const { data } = await axios.get("/api/teams?team_id=" + teamId);
+    if (data.success) {
+      setTeam(data.userTeams[0]);
+    }
+  };
+
+  useEffect(() => {
+    if (session?.userId) {
+      handleFetchTeams();
+    }
+  }, [session]);
+
   return (
     <>
       <div className="flex-1 space-y-4  p-4 md:p-8 pt-6">
         <BreadCrumb items={breadcrumbItems} />
 
         <div className="flex items-start justify-between">
-          <Heading
-            title={`Team settings`}
-            description={`Manage your repositories`}
-          />
+          {team == null ? (
+            <Skeleton className="h-10 w-[520px]" />
+          ) : (
+            <Heading
+              title={`${team ? team.name : ""} settings`}
+              description={`Manage your repositories`}
+            />
+          )}
         </div>
         <Separator />
 
