9df4350775edd86bfd4d708e33eebc00257fddd4
Gabriel Ferraz <sudoferraz@gmail.com>
2024-03-26 22:58:21 +0200

    Fixes onboarding status and redirects with darkmode issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,10 @@ import { UserOnboarding } from "@prisma/client";
 import axios from "axios";
 import { redirect } from "next/navigation";
 import { useEffect, useState } from "react";
+import {
+  OnboardingFlow,
+  OnboardingStep,
+} from "./api/onboarding/userOnboardingService";
 
 export default function Home() {
   const [userOnboardingStatus, setUserOnboarding] = useState<UserOnboarding>();
@@ -15,17 +19,25 @@ export default function Home() {
     }
   };
 
+  const handleUpdateUserOnboarding = async () => {
+    await axios.post("/api/onboarding", {
+      onboardingFlow: OnboardingFlow.BETA,
+      onboardingStep: OnboardingStep.COMPLETED,
+    });
+  };
+
   useEffect(() => {
     handleFetchUserOnboarding();
   }, []);
 
   useEffect(() => {
-    // if (userOnboardingStatus?. === "completed") {
-    //   redirect("/repositories/new");
-    // } else {
-    redirect("/overview");
-    // }
+    if (userOnboardingStatus) {
+      if (userOnboardingStatus.flow.toLowerCase() !== "beta") {
+        handleUpdateUserOnboarding();
+        redirect("/repositories/new");
+      } else {
+        redirect("/overview");
+      }
+    }
   }, [userOnboardingStatus]);
-
-  redirect("/overview");
 }
