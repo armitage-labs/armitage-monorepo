1fa1e017c55f7b284bfe086f25a730585d8eb99a
Gabriel Ferraz <sudoferraz@gmail.com>
2024-03-26 23:24:44 +0200

    Reverts the redirect based on onboarding for now (#79)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,43 +1,44 @@
 "use client";
 
-import { UserOnboarding } from "@prisma/client";
-import axios from "axios";
+// import { UserOnboarding } from "@prisma/client";
+// import axios from "axios";
 import { redirect } from "next/navigation";
-import { useEffect, useState } from "react";
-import {
-  OnboardingFlow,
-  OnboardingStep,
-} from "./api/onboarding/userOnboardingService";
+// import { useEffect, useState } from "react";
+// import {
+//   OnboardingFlow,
+//   OnboardingStep,
+// } from "./api/onboarding/userOnboardingService";
 
 export default function Home() {
-  const [userOnboardingStatus, setUserOnboarding] = useState<UserOnboarding>();
-
-  const handleFetchUserOnboarding = async () => {
-    const { data } = await axios.get("/api/onboarding");
-    if (data.success) {
-      setUserOnboarding(data.onboardingStatus);
-    }
-  };
-
-  const handleUpdateUserOnboarding = async () => {
-    await axios.post("/api/onboarding", {
-      onboardingFlow: OnboardingFlow.BETA,
-      onboardingStep: OnboardingStep.COMPLETED,
-    });
-  };
-
-  useEffect(() => {
-    handleFetchUserOnboarding();
-  }, []);
-
-  useEffect(() => {
-    if (userOnboardingStatus) {
-      if (userOnboardingStatus.flow.toLowerCase() !== "beta") {
-        handleUpdateUserOnboarding();
-        redirect("/repositories/new");
-      } else {
-        redirect("/overview");
-      }
-    }
-  }, [userOnboardingStatus]);
+  // const [userOnboardingStatus, setUserOnboarding] = useState<UserOnboarding>();
+  //
+  // const handleFetchUserOnboarding = async () => {
+  //   const { data } = await axios.get("/api/onboarding");
+  //   if (data.success) {
+  //     setUserOnboarding(data.onboardingStatus);
+  //   }
+  // };
+  //
+  // const handleUpdateUserOnboarding = async () => {
+  //   await axios.post("/api/onboarding", {
+  //     onboardingFlow: OnboardingFlow.BETA,
+  //     onboardingStep: OnboardingStep.COMPLETED,
+  //   });
+  // };
+  //
+  // useEffect(() => {
+  //   handleFetchUserOnboarding();
+  // }, []);
+  //
+  // useEffect(() => {
+  //   if (userOnboardingStatus) {
+  //     if (userOnboardingStatus.flow.toLowerCase() !== "beta") {
+  //       handleUpdateUserOnboarding();
+  //       redirect("/repositories/new");
+  //     } else {
+  //       redirect("/overview");
+  //     }
+  //   }
+  // }, [userOnboardingStatus]);
+  redirect("/overview");
 }
