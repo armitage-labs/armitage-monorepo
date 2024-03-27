7ea39e0e02d7ce5902c5a704a7dafd3568571c19
Gabriel Ferraz <sudoferraz@gmail.com>
2024-03-27 23:08:05 +0200

    Adds explainer tooltips and tabs for repository details page regarding contributors table

--- src/app/(dashboard)/teams/[teamId]/teamInsights.tsx
+++ src/app/(dashboard)/teams/[teamId]/teamInsights.tsx
@@ -1,6 +1,7 @@
 import { Icons } from "@/components/icons";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { OverviewDto } from "@/app/api/teams/types/overview.dto";
+import { HoverExplainer } from "@/components/hoverExplainer";
 
 interface TeamInsightsProps {
   overview?: OverviewDto;
@@ -12,7 +13,7 @@ export function TeamInsights({ overview }: TeamInsightsProps) {
       <Card>
         <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
           <CardTitle className="text-sm font-medium">
-            Total team impact
+            Total impact
           </CardTitle>
           <Icons.lineChart></Icons.lineChart>
         </CardHeader>
@@ -20,16 +21,24 @@ export function TeamInsights({ overview }: TeamInsightsProps) {
           <div className="text-2xl font-bold">
             {overview?.totalScore.toFixed(2)}
           </div>
-          <p className="pt-1 text-xs text-muted-foreground">
-            +{overview?.weekGrowth.toFixed(2)}% CRED earned
-          </p>
+          <div className="flex justify-between">
+            <p className="pt-1 text-xs text-muted-foreground">
+              +{overview?.weekGrowth.toFixed(2)}% impact this week
+            </p>
+            <HoverExplainer
+              title="How the current week compares to the total engagement and impact"
+              description="The total engagement is the sum of all the scores of the contributors in your team"
+              subtitle="Calculated from the first activity of a team."
+              imageSrc="https://cdn11.bigcommerce.com/s-hii7479o/images/stencil/original/products/13868/32254/pizza__85111.1568154248.png?c=2"
+            />
+          </div>
         </CardContent>
       </Card>
 
       <Card>
         <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
           <CardTitle className="text-sm font-medium">
-            Team impact earned week
+            Team engagement this week
           </CardTitle>
           <Icons.pizza></Icons.pizza>
         </CardHeader>
@@ -37,10 +46,18 @@ export function TeamInsights({ overview }: TeamInsightsProps) {
           <div className="text-2xl font-bold">
             {overview?.weekScore.toFixed(2)}
           </div>
-          <p className="pt-1 text-xs text-muted-foreground">
-            {overview?.wowScore ? (overview?.wowScore > 0 ? "+" : "-") : ""}{" "}
-            {overview?.wowScore.toFixed(2)}% over last week
-          </p>
+          <div className="flex justify-between">
+            <p className="pt-1 text-xs text-muted-foreground">
+              {overview?.wowScore ? (overview?.wowScore > 0 ? "+" : "-") : ""}{" "}
+              {overview?.wowScore.toFixed(2)}% over last week
+            </p>
+            <HoverExplainer
+              title="How the current week compares to the previous week"
+              description="The team engagement score of this week is the sum of all the scores of the contributors in your team"
+              subtitle="Calculated on a rolling Sunday to Sunday basis"
+              imageSrc="https://cdn-icons-png.flaticon.com/512/7172/7172779.png"
+            />
+          </div>
         </CardContent>
       </Card>
 
--- src/components/overview/weeklyImpactCard.tsx
+++ src/components/overview/weeklyImpactCard.tsx
@@ -1,6 +1,7 @@
 import { OverviewDto } from "@/app/api/teams/types/overview.dto";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Icons } from "@/components/icons";
+import { HoverExplainer } from "../hoverExplainer";
 
 interface WeeklyImpactCardProps {
   overview?: OverviewDto;
@@ -10,17 +11,25 @@ export function WeeklyImpactCard({ overview }: WeeklyImpactCardProps) {
   return (
     <Card>
       <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-        <CardTitle className="text-sm font-medium">Impact this week</CardTitle>
+        <CardTitle className="text-sm font-medium">Engagement this week</CardTitle>
         <Icons.gitBranch></Icons.gitBranch>
       </CardHeader>
       <CardContent>
         <div className="text-2xl font-bold">
           {overview?.weekScore?.toFixed(2) || 0}
         </div>
-        <p className="pt-1 text-xs text-muted-foreground">
-          {overview?.wowScore ? (overview?.wowScore > 0 ? "+" : "") : ""}{" "}
-          {overview?.wowScore?.toFixed(2) || 0}% compared to last week
-        </p>
+        <div className="flex justify-between">
+          <p className="pt-1 text-xs text-muted-foreground">
+            {overview?.wowScore ? (overview?.wowScore > 0 ? "+" : "") : ""}{" "}
+            {overview?.wowScore?.toFixed(2) || 0}% compared to last week
+          </p>
+          <HoverExplainer
+            title="Understand how the current week compares to the previous week"
+            description="The weekely engagement is the sum of all the scores of the contributors in all your teams during the current week."
+            subtitle="Calculated from Sunday to Sunday."
+            imageSrc="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Git_icon.svg/2048px-Git_icon.svg.png"
+          />
+        </div>
       </CardContent>
     </Card>
   );
