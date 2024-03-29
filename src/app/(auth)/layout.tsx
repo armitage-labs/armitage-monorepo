5d77be62848e4b06e7a78f672656c725e5383051
Gabriel Ferraz <sudoferraz@gmail.com>
2024-03-29 16:23:13 +0200

    Adds calculate button on team details page (#90)
    * Adds calculate button on team details page
    
    * Fixes unrelated linter issue

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,8 +1,5 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "../globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
