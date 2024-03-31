79e3d14126b812fe8ef68695fa7fe0ac8737c4d0
Gabriel Ferraz <sudoferraz@gmail.com>
2024-03-31 16:38:19 +0300

    Adds DB migration for team weight configs (#91)

---
+++ prisma/migrations/20240331133511_teamweights/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE "TeamWeightConfig" (
+    "id" TEXT NOT NULL,
+    "type" VARCHAR(255) NOT NULL,
+    "value" DOUBLE PRECISION NOT NULL,
+    "team_id" TEXT NOT NULL,
+
+    CONSTRAINT "TeamWeightConfig_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "TeamWeightConfig_team_id_key" ON "TeamWeightConfig"("team_id");
+
+-- AddForeignKey
+ALTER TABLE "TeamWeightConfig" ADD CONSTRAINT "TeamWeightConfig_team_id_fkey" FOREIGN KEY ("team_id") REFERENCES "Team"("id") ON DELETE CASCADE ON UPDATE CASCADE;
