57637ca70978c6d6a528b89859ca32ee598b5acb
Alec <alecerasmus2@gmail.com>
2024-04-09 16:47:32 +0200

    Fix build issues

--- src/app/api/user-metrics/route.ts
+++ src/app/api/user-metrics/route.ts
@@ -10,7 +10,7 @@ export async function GET(req: NextRequest) {
   const teamId = req.nextUrl.searchParams.get("team_id");
   const username = req.nextUrl.searchParams.get("username");
   const repoName = req.nextUrl.searchParams.get("repo_name");
-  if (teamId && username) {
+  if (teamId && username && repoName) {
     const teamUserMetrics = await fetchTeamUserMetrics(teamId, repoName);
     const topTeamMetrics = await feachMaxTeamMetrics(teamUserMetrics);
     const userMetrics = await feachUsersTeamMetrics(username, teamUserMetrics);
