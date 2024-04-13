8b1348b7e16e7327e9c0286bb023c3fc5ca644d6
Gabriel Ferraz <sudoferraz@gmail.com>
2024-04-13 16:10:38 +0200

    Fixes linter issues

--- src/app/(dashboard)/attestation/attestationModal.tsx
+++ src/app/(dashboard)/attestation/attestationModal.tsx
@@ -31,9 +31,9 @@ export function GenerateAttestationModal({
   const session = useSession();
   const [userAddress, setUserAddress] = useState<string | undefined>(undefined);
   const [attestationPrivateData, setAttestationPrivateData] = useState<any>();
-  const [registeredAttestationUuid, setRegisteredAttestationUuid] = useState<string | undefined>(
-    undefined,
-  );
+  const [registeredAttestationUuid, setRegisteredAttestationUuid] = useState<
+    string | undefined
+  >(undefined);
   const [userSalt, setUserSalt] = useState<string | undefined>(undefined);
   const [userLogin, setUserLogin] = useState<string | undefined>(undefined);
 
@@ -85,12 +85,10 @@ export function GenerateAttestationModal({
   };
 
   const handlePostAttestationCreated = async (attestationUuid: string) => {
-    const { data } = await axios.post("/api/attestations",
-      {
-        chain_id: "11155111",
-        attestation_uuid: attestationUuid
-      }
-    );
+    const { data } = await axios.post("/api/attestations", {
+      chain_id: "11155111",
+      attestation_uuid: attestationUuid,
+    });
     if (data.success) {
       setRegisteredAttestationUuid(attestationUuid);
     }
@@ -119,35 +117,33 @@ export function GenerateAttestationModal({
         <DialogHeader>
           <DialogTitle>Creating your private attestation on-chain</DialogTitle>
           <DialogDescription>
-            You will be prompted to sign a message and broadcast it to create your attestation
+            You will be prompted to sign a message and broadcast it to create
+            your attestation
           </DialogDescription>
         </DialogHeader>
         <div className="flex flex-col items-center space-x-2 pt-6 pb-6">
-          {registeredAttestationUuid
-            ? (
-              <div>
-                Your private attestation has been created 🥳
+          {registeredAttestationUuid ? (
+            <div>
+              Your private attestation has been created 🥳
+              <div className="pt-6">
+                <Link
+                  href={`https://sepolia.easscan.org/attestation/view/${registeredAttestationUuid}`}
+                >
+                  <Button variant={"outline"} onClick={() => {}}>
+                    Open in EAS
+                  </Button>
+                </Link>
                 <div className="pt-6">
-                  <Link href={`https://sepolia.easscan.org/attestation/view/${registeredAttestationUuid}`}>
-                    <Button
-                      variant={"outline"}
-                      onClick={() => {
-                      }}
-                    >Open in EAS</Button>
-                  </Link>
-                  <div className="pt-6">
-                    <Button>Generate Proofs</Button>
-                  </div>
+                  <Button>Generate Proofs</Button>
                 </div>
               </div>
-            )
-            : (
-              <LoadingCircle></LoadingCircle>
-            )
-          }
+            </div>
+          ) : (
+            <LoadingCircle></LoadingCircle>
+          )}
         </div>
         <DialogFooter className="sm:justify-start">
-          <DialogClose asChild onClick={() => { }}></DialogClose>
+          <DialogClose asChild onClick={() => {}}></DialogClose>
         </DialogFooter>
       </DialogContent>
     </Dialog>
