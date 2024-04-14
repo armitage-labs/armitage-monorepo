71dc07d84e1eb16a7473c867c6c273224dc04f61
Gabriel Ferraz <sudoferraz@gmail.com>
2024-04-14 08:46:26 +0200

    Adds tracking on signin (#115)

--- src/components/authentication/user-auth-form.tsx
+++ src/components/authentication/user-auth-form.tsx
@@ -7,6 +7,7 @@ import { Button } from "../ui/button";
 import { Label } from "../ui/label";
 import { signIn } from "next-auth/react";
 import { Icons } from "../icons";
+import { track } from "@vercel/analytics/react";
 
 interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
 
@@ -35,7 +36,10 @@ export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
             className="w-full"
             variant="outline"
             type="button"
-            onClick={() => signIn("github", { callbackUrl: "/overview" })}
+            onClick={() => {
+              track("clicked_sign_in");
+              signIn("github", { callbackUrl: "/overview" });
+            }}
           >
             <Icons.gitHub className="mr-2 h-4 w-4" />
             Continue with Github
