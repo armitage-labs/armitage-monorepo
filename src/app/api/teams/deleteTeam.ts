b868f58b080aa808c2e044387b3c54c0b1a0f80b
Gabriel Ferraz <sudoferraz@gmail.com>
2024-04-16 21:30:49 +0300

    Adds feature to delete teams

---
+++ src/app/api/teams/deleteTeam.ts
@@ -0,0 +1,16 @@
+import prisma from "db";
+
+
+export async function deleteTeam(teamId: string, userId: string): Promise<boolean> {
+  try {
+    await prisma.team.delete({
+      where: {
+        id: teamId,
+        owner_user_id: userId,
+      },
+    });
+    return true;
+  } catch (error) {
+    return false;
+  }
+}
