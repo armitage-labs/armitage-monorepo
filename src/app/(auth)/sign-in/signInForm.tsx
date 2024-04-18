a03c6e38136399bac1a0d370f7cc9d11a4eac9ed
Alec <alecerasmus2@gmail.com>
2024-04-18 16:28:00 +0200

    Lint issues

--- src/app/(auth)/sign-in/signInForm.tsx
+++ src/app/(auth)/sign-in/signInForm.tsx
@@ -8,56 +8,56 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 export default function SignInForm() {
-    const [anonymousUserUid, setAnonymousUserUid] = useState<string | null>(null);
-    const [email, setEmail] = useState<string>("");
-    const [isLoading, setIsLoading] = useState(true);
+  const [anonymousUserUid, setAnonymousUserUid] = useState<string | null>(null);
+  const [email, setEmail] = useState<string>("");
+  const [isLoading, setIsLoading] = useState(true);
 
-    const handleSignInForm = async () => {
-        localStorage.setItem("anonymousUserUid", email);
-        setAnonymousUserUid(email);
-        const { data } = await axios.post("/api/users", {
-            email: email,
-        });
+  const handleSignInForm = async () => {
+    localStorage.setItem("anonymousUserUid", email);
+    setAnonymousUserUid(email);
+    const { data } = await axios.post("/api/users", {
+      email: email,
+    });
 
-        if (data?.id) {
-            localStorage.setItem("anonymousUserUid", data.id);
-            setAnonymousUserUid(data.id);
-        }
-    };
+    if (data?.id) {
+      localStorage.setItem("anonymousUserUid", data.id);
+      setAnonymousUserUid(data.id);
+    }
+  };
 
-    useEffect(() => {
-        const storedValue = localStorage.getItem("anonymousUserUid");
-        if (storedValue) {
-            setAnonymousUserUid(storedValue);
-        }
-        setIsLoading(false);
-    }, []);
+  useEffect(() => {
+    const storedValue = localStorage.getItem("anonymousUserUid");
+    if (storedValue) {
+      setAnonymousUserUid(storedValue);
+    }
+    setIsLoading(false);
+  }, []);
 
-    return (
+  return (
+    <>
+      {isLoading ? (
         <>
-            {isLoading ? (
-                <>
-                    <Skeleton className="h-12 w-100" />
-                </>
-            ) : (
-                <>
-                    {anonymousUserUid ? (
-                        <UserAuthForm />
-                    ) : (
-                        <>
-                            <Input
-                                type="email"
-                                placeholder="Email"
-                                onChange={(event) => setEmail(event.target.value)}
-                                value={email}
-                            />
-                            <Button variant={"secondary"} onClick={() => handleSignInForm()}>
-                                <>Sign in</>
-                            </Button>
-                        </>
-                    )}
-                </>
-            )}
+          <Skeleton className="h-12 w-100" />
         </>
-    );
+      ) : (
+        <>
+          {anonymousUserUid ? (
+            <UserAuthForm />
+          ) : (
+            <>
+              <Input
+                type="email"
+                placeholder="Email"
+                onChange={(event) => setEmail(event.target.value)}
+                value={email}
+              />
+              <Button variant={"secondary"} onClick={() => handleSignInForm()}>
+                <>Sign in</>
+              </Button>
+            </>
+          )}
+        </>
+      )}
+    </>
+  );
 }
