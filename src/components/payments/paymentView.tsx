eaa9fe73a804216a12086cf35d7287f96408ae03
Alec <alecerasmus2@gmail.com>
2024-05-13 14:48:28 +0200

    Move balance to own comp

--- src/components/payments/paymentView.tsx
+++ src/components/payments/paymentView.tsx
@@ -1,19 +1,11 @@
-import { Button } from "@/components/ui/button";
-import { Split, Wallet2 } from "lucide-react";
+import { Split } from "lucide-react";
 import { Heading } from "@/components/ui/heading";
-import {
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { useEffect } from "react";
-import { useSplitEarnings } from "@0xsplits/splits-sdk-react";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import SplitsRecipients from "./splits/splitsRecipients";
 import { PaymentAddressDto } from "@/app/api/payments/route";
 import { chainsConfig } from "@/app/(dashboard)/attestation/utils/attestation-config";
 import { truncateString } from "@/app/(dashboard)/utils/stringUtils";
+import { SplitsBalance } from "./splits/splitsBalance";
 
 interface PaymentsViewProps {
   projectId: string;
@@ -41,19 +33,6 @@ export default function PaymentsView({
   projectId,
   paymentAddress,
 }: PaymentsViewProps) {
-  const { splitEarnings, isLoading, status, error } = useSplitEarnings(
-    parseInt(paymentAddress.chain_id),
-    paymentAddress.wallet_address,
-    true,
-    [paymentAddress.wallet_address],
-  );
-
-  useEffect(() => {
-    if (splitEarnings != null) {
-      console.log(splitEarnings);
-    }
-  }, [status]);
-
   return (
     <div className="flex-1 space-y-4  p-4 md:p-8 pt-6">
       <div className="flex items-start">
@@ -76,35 +55,10 @@ export default function PaymentsView({
       </div>
       <div className="grid grid-cols-2 gap-4">
         <div>
-          <Card>
-            <CardHeader>
-              <div className="flex items-center">
-                <Wallet2 className="inline-block" />
-                <CardTitle className="inline-block ml-2">Balance</CardTitle>
-              </div>
-            </CardHeader>
-            <CardContent>
-              <div className="flex justify-center">
-                <div className="text-6xl font-bold">$0.00</div>
-              </div>
-              <div className="border-t border-gray-300 mt-3 mb-3"></div>
-              <div className="flex justify-center">
-                <p className="text-xl text-muted-foreground">
-                  No ETH or ERC-20
-                </p>
-              </div>
-              <div className="border-t border-gray-300 mt-3 mb-3"></div>
-            </CardContent>
-            <CardFooter>
-              <Button
-                className="w-full text-md flex justify-between items-center"
-                onClick={() => console.log()}
-              >
-                Distribute Balances
-                <Split className="mr-2 h-5 w-5 transform rotate-90" />
-              </Button>
-            </CardFooter>
-          </Card>
+          <SplitsBalance
+            projectId={projectId}
+            paymentAddress={paymentAddress}
+          ></SplitsBalance>
         </div>
         <div>
           <Card>
---
+++ src/components/payments/splits/splitsBalance.tsx
@@ -0,0 +1,71 @@
+import { PaymentAddressDto } from "@/app/api/payments/route";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { useSplitEarnings } from "@0xsplits/splits-sdk-react";
+import { Split, Wallet2 } from "lucide-react";
+import { useEffect, useState } from "react";
+
+interface SplitsBalanceProps {
+  projectId: string;
+  paymentAddress: PaymentAddressDto;
+}
+
+export function SplitsBalance({
+  projectId,
+  paymentAddress,
+}: SplitsBalanceProps) {
+  const { splitEarnings, isLoading, status, error } = useSplitEarnings(
+    parseInt(paymentAddress.chain_id),
+    paymentAddress.wallet_address,
+    true,
+    [paymentAddress.wallet_address],
+  );
+  const [balance, setBalance] = useState<string>("0.00");
+  useEffect(() => {
+    if (splitEarnings != null) {
+      const formattedAmount =
+        splitEarnings.activeBalances?.[
+          "0x0000000000000000000000000000000000000000"
+        ]?.formattedAmount;
+      if (formattedAmount) {
+        setBalance(formattedAmount);
+      }
+    }
+  }, [status]);
+
+  return (
+    <Card>
+      <CardHeader>
+        <div className="flex items-center">
+          <Wallet2 className="inline-block" />
+          <CardTitle className="inline-block ml-2">Balance</CardTitle>
+        </div>
+      </CardHeader>
+      <CardContent>
+        <div className="flex justify-center">
+          <div className="text-6xl font-bold">ETH {balance}</div>
+        </div>
+        <div className="border-t border-gray-300 mt-3 mb-3"></div>
+        <div className="flex justify-center">
+          <p className="text-xl text-muted-foreground">No ETH or ERC-20</p>
+        </div>
+        <div className="border-t border-gray-300 mt-3 mb-3"></div>
+      </CardContent>
+      <CardFooter>
+        <Button
+          className="w-full text-md flex justify-between items-center"
+          onClick={() => console.log()}
+        >
+          Distribute Balances
+          <Split className="mr-2 h-5 w-5 transform rotate-90" />
+        </Button>
+      </CardFooter>
+    </Card>
+  );
+}
